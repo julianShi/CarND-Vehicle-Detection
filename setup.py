b2e3d61e998e3a99e16815d5542e5b0b5ef8c025
Ubuntu <ubuntu@ip-172-31-7-36.ca-central-1.compute.internal>
2018-04-22 15:18:03 +0000

    environment setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Vehicle-Detection',
+  version='1.0',
+  description='Install dependencies',
+  author='Julian Shi',
+  author_email='',
+  url='',
+  packages=[],
+  install_requires=[
+    'requests',
+    'moviepy',
+    'numpy',
+    'scipy',
+    'opencv-python',
+    'scikit-image',
+    'scikit-learn'
+  ],
+)
+
